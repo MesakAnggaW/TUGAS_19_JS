d3c2f4c46a12924a6f00791ab9ca7e1ae632558d
wccXpfhc <89910124+MesakAnggaW@users.noreply.github.com>
2022-12-18 09:05:58 +0700

    Mengerjakan Tugas 19 Javascript

---
+++ tugas19.js
@@ -0,0 +1,18 @@
+// let kalimat = "Apa kabar kamu? bagaimana ia menjagamu";
+
+// console.log(kalimat.includes("Apa", 1));
+// console.log(kalimat.endsWith("mu"));
+// console.log(kalimat.startsWith("Apa ka"));
+// console.log(kalimat.repeat(3));
+
+let kalimat = "Semangat...";
+let bintang = "*******";
+
+console.log(kalimat.repeat(4));
+console.log(bintang);
+console.log(bintang.repeat(2));
+console.log(bintang.repeat(4));
+console.log(bintang.repeat(8));
+console.log(bintang.repeat(4));
+console.log(bintang.repeat(2));
+console.log(bintang);
